da4c9bc20b1377a5536950908426bb2cf8466285
LanceFK <72005857+LanceFK@users.noreply.github.com>
2022-06-08 13:36:32 -0700

    Inside edit_profile.html-added user social media links, profile pic, progile bio and resized images

--- ShoeN/models.py
+++ ShoeN/models.py
@@ -1,3 +1,4 @@
+import profile
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -12,6 +13,21 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('home')
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_pic =  models.ImageField(null=True, blank=True, upload_to='images/profile/')
+    website_url = models.CharField(max_length=255, null=True, blank=True)
+    instagram_url = models.CharField(max_length=255, null=True, blank=True)
+    twitter_url = models.CharField(max_length=255, null=True, blank=True)
+    facebook_url = models.CharField(max_length=255, null=True, blank=True)
+    pinterest_url = models.CharField(max_length=255, null=True, blank=True)
+
+
+    def __str__(self):
+        return str(self.user)
+
 
 
 class Post(models.Model):
