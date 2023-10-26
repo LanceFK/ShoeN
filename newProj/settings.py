38f73d2726dcf36039a3a596327318ed4c81cf21
LanceFK <72005857+LanceFK@users.noreply.github.com>
2023-10-26 13:02:57 -0700

    settings.py - added port number to database info.

--- newProj/settings.py
+++ newProj/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'HOST': os.environ.get("HOST"),
         'USER': os.environ.get("USER"),
         'PASSWORD':  os.environ.get("PASSWORD"), 
-        'PORT': '',
+        'PORT': '5432',
         'OPTIONS': {'sslmode': 'require'},
     }
 }
