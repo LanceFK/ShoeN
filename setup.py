c25101b242b2ba592de0bb1f6309788583b9f452
Lance Kinchen <lfk2682@gmail.com>
2024-02-27 17:29:38 -0800

    created setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    
+    name='SHOEN_APP',
+    version='1.0',
+    author='Lance F Kinchen',
+    author_email='lfk2682@gmail.com',
+    packages='find_namespace_packages()',
+    
+)
