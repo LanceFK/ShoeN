d4aaeac901288757e8afb4c7b4fc57fc1d37bdf2
Lance Kinchen <lfk2682@gmail.com>
2024-02-27 17:36:57 -0800

    setup.py test2

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version='1.0',
     author='Lance F Kinchen',
     author_email='lfk2682@gmail.com',
-    packages='find_namespace_packages()',
+    packages='greenlet.platform',
+    find_namespace_packages='find_namespace_packages()',
     
 )
