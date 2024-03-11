6d222ead7e4aa845973603c9657683559f97c67a
Lance Kinchen <lfk2682@gmail.com>
2024-03-11 11:17:55 -0700

    packages

---
+++ ShoeN-1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, greenlet
+
+setup(
+    
+    name='SHOEN_APP',
+    version='1.0',
+    author='Lance F Kinchen',
+    author_email='lfk2682@gmail.com',
+    packages='greenlet',
+    
+)
